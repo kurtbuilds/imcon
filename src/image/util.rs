340a6420522632f52a200ae77c7af380cd3f3842
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-02-16 11:23:15 -0500

    fix

--- src/image/util.rs
+++ src/image/util.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
 
 pub fn create_path(path_template: &str, input_path: &PathBuf, page: usize, n_pages: usize) -> String {
+    let places = n_pages.to_string().len();
     path_template
         .replace("{}", input_path.file_stem().unwrap().to_string_lossy().as_ref())
-        .replace("{i}", format!("{:0places$}", page, places = n_pages).as_ref())
+        .replace("{i}", format!("{:0places$}", page, places = places).as_ref())
 }
